41977e4adb2057fae71ee2932442207b9ac5c471
Luca Paganelli <luca.paga@gmail.com>
2017-12-17 23:37:47 +0100

    mitticco

--- pubsub/03_cli_subscriber.sh
+++ pubsub/03_cli_subscriber.sh
@@ -2,4 +2,12 @@
 
 . ../00_set_params.sh
 
-gcloud beta pubsub subscriptions pull --auto-ack --max-messages=100 chat-$V-subscription
+if [ "$1" == "loop" ]
+then
+  while true; do
+    gcloud beta pubsub subscriptions pull --auto-ack --limit 10 chat-$V-subscription
+    sleep 2
+  done
+else
+  gcloud beta pubsub subscriptions pull --auto-ack --limit 100 chat-$V-subscription
+fi
