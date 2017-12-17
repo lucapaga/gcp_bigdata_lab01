9cac999f672c1f6bddea4904254d0d4febc5dcad
Luca Paganelli <luca.paga@gmail.com>
2017-12-17 23:21:54 +0100

    moving to CLI

--- pubsub/01_start_subscriber.sh
+++ pubsub/01_start_subscriber.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 . ../00_set_params.sh
-python chat.py --subscription_name chat-$V-subscription --mode consume
+python chat.py --topic_name chat-$V --subscription_name chat-$V-subscription --mode consume
