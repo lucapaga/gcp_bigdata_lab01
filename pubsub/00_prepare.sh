938dc381d78041fb1cd138f0b4a8dc7bb3de69b5
Luca Paganelli <luca.paga@gmail.com>
2017-12-17 23:42:14 +0100

    more explicative

--- pubsub/00_prepare.sh
+++ pubsub/00_prepare.sh
@@ -4,6 +4,9 @@
 gcloud beta pubsub topics create chat-$V
 gcloud beta pubsub subscriptions create chat-$V-subscription --topic chat-$V
 
-echo "USE THE FOLLOWING SUBSCRIPTION NAME: 'chat-$V-subscription'"
-
-echo "NOW HIT 'source ../src/python/mqtt_client/env/bin/activate' AT FIRST"
+echo ""
+echo "----------------------------------"
+echo "         TOPIC: 'chat-$V'"
+echo "  SUBSCRIPTION: 'chat-$V-subscription'"
+echo "----------------------------------"
+echo ""
